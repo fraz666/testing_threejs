5363c36b3dbe3fff4a3cac5211e3ebf24cae89b5
NikPiermafrost <pierma.nick@gmail.com>
2023-12-22 13:30:25 +0100

    removed redis

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,12 +8,16 @@ const httpServer = createServer(app);
 const gs = gameServer(httpServer);
 
 app.use(express.json());
+
+// req.gameServer accessibile con cast tramite AppRequest
 app.use('gameServer', (req, res, next) => {
   (req as AppRequest).gameServer = gs;
   next();
 });
 
 app.get('/', async (req, res) => {
+  const { gameServer } = req as AppRequest;
+
   res.sendFile(__dirname + '/index.html');
 });
 
