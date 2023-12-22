cd073fe4b9a0384993d2121f238bbe461e1ee431
NikPiermafrost <pierma.nick@gmail.com>
2023-12-22 13:25:49 +0100

    optional redis

--- server/utils/socket.ts
+++ server/utils/socket.ts
@@ -3,7 +3,7 @@ import http from 'node:http';
 
 const gameServer = (httpServer: http.Server) => new Server({
   server: httpServer,
-  presence: new RedisPresence(process.env.REDIS_URL),
+  presence: process.env.REDIS_URL ? new RedisPresence(process.env.REDIS_URL) : undefined,
   devMode: process.env.NODE_ENV !== 'production'
 });
 
